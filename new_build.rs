977354f6a4048fb3be80dde9818b2c13ca5e72ba
uff20xd <95922184+uff20xd@users.noreply.github.com>
2025-07-01 11:32:40 +0000

    Update new_build.rs

--- new_build.rs
+++ new_build.rs
@@ -4,16 +4,16 @@ use redoxr::redoxr::*;
 const COMMOM_FLAGS: &[&str] = &["-O"];
 
 fn main () -> () {
-    let mut redoxr = Redoxr::new()
-        .common_flags(COMMON_FLAGS);
+    
+    let mut redoxr = Redoxr::new();
 
     let mut clap = RedoxCrate::new(&mut redoxr, "clap")
-        .flag("-O");
+        .flag(COMMON_FLAGS);
 
     let main_crate = RedoxCrate::main(&mut redoxr, "worseSaltz", "src")
         .dependency(clap)
         .flag(&["-O"]);
 
     
-    if build_script.compile() {return ();}
+    if redoxr.compile() {return ();}
 }
