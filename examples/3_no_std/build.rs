d8e21cd1cfb5b5ad700b51c21d7453e43016d851
uff20xd <donut121008@gmail.com>
2025-07-11 16:24:24 +0200

    k

--- examples/3_no_std/build.rs
+++ examples/3_no_std/build.rs
@@ -9,22 +9,22 @@ fn main() -> MainResult {
 
     let mut main_crate = RustCrate::new("nostd", ".")
         .flags(&[
-            "--edition", "2024",
-            //"-Copt-level=z",
+            "-Copt-level=z",
             "-Cpanic=abort",
-            "-l", "c",
+            //"-l", "c",
             //"-Clink-arg=-nostartfiles",
             //"-Ctarget-feature=+crt-static",
+            "--target=x86_64-unknown-linux-gnu",
             "--target=x86_64-unknown-none",
-            //"--emit=obj"
         ])
         .make_bin()
+        .set_output_file("nostd")
         .make_output()
-        //.set_output_file("nostd.o")
         .stay();
 
-    let _ = Cmd::new("gcc").args(&["src/test.c", "-o", "out/test"]).spawn()?.wait()?;
     compile!(main_crate);
+    let _ = Cmd::new("gcc").args(&["src/test.c", "-o", "out/test"]).status()?;
+
     run!(main_crate);
     Ok(())
 }
