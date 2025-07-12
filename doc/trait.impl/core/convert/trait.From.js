3192c55648efda578130f0a34f235fdc97bda0a1
uff20xd <donut121008@gmail.com>
2025-07-12 15:19:05 +0200

    could probably compile a cargo as dependency now

---
+++ doc/trait.impl/core/convert/trait.From.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["redoxr",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.87.0/std/io/error/struct.Error.html\" title=\"struct std::io::error::Error\">Error</a>&gt; for <a class=\"enum\" href=\"redoxr/redoxr/enum.RedoxError.html\" title=\"enum redoxr::redoxr::RedoxError\">RedoxError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[428]}
